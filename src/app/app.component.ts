3d367029d8a47c0ed3368b8b7c820326b1bc68bc
Girgisnasim <gergesnaseem53@gmail.com>
2024-03-27 13:37:05 +0200

    import routeing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./Components/login/login.component";
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+
 
 
 @Component({
@@ -15,7 +16,8 @@ import { FooterComponent } from './Components/footer/footer.component';
       RouterOutlet,
       LoginComponent,
       HeaderComponent,
-      FooterComponent
+      FooterComponent,
+      RouterModule
     ]
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: 'home', loadChildren: './home/home.module#HomeModule' },
+    // { path: 'employees', loadChildren: './employees/employees.module#EmployeesModule' },
+    // { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule' },
+    // { path: 'projects/:id', loadChildren: './projects/projects.module#ProjectsModule' },
+    // { path: 'projects/:id/tasks', loadChildren: './tasks/tasks.module#TasksModule' },
+    // { path: 'projects/:id/tasks/:id', loadChildren: './tasks/tasks.module#TasksModule' },
+    // { path: 'projects/:id/tasks/:id/edit', loadChildren: './tasks/tasks.module#TasksModule' },
+];
