1bdb71b179df1ad40dbd210acc51dc01460f48f6
Girgisnasim <gergesnaseem53@gmail.com>
2024-03-27 13:33:53 +0200

    add header and footer componetes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./Components/login/login.component";
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [
+      RouterOutlet,
+      LoginComponent,
+      HeaderComponent,
+      FooterComponent
+    ]
 })
 export class AppComponent {
   title = 'HrManagementSystem';
